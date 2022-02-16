136c9daabdec98f3713ae4d5ffb0f378717614a9
Tadeo Rivera <xtecuan@gmail.com>
2022-02-16 17:17:46 -0600

    cambios deploy

---
+++ temporal/src/main/java/com/xtesoft/tienda/resources/app/RestApplication.java
@@ -0,0 +1,8 @@
+package  com.xtesoft.tienda.resources.app;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+}
