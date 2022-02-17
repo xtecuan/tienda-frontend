24adc5d9cf74fb89e2bdb3e9b26d13acc95030a4
Tadeo Rivera <xtecuan@gmail.com>
2022-02-16 23:27:36 -0600

    cambios deploy

--- src/main/java/com/xtesoft/tienda/clientes/TiendaApiService.java
+++ src/main/java/com/xtesoft/tienda/clientes/TiendaApiService.java
@@ -9,7 +9,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/api/clientes")
+@Path("/clientes")
 @Produces("application/json")
 @Consumes("application/json")
 @RegisterRestClient
